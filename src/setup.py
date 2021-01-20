c2a88c198700330967c0a347f41c21975343436b
Luis Youn <yhy0215@gmail.com>
2021-01-21 09:26:43 +1100

    add setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FED_FinBert",
+    version="0.1.0",
+    author="Hoyoul Youn",
+    author_email="yhy0215@gmail.com",
+    url="https://github.com/YounHoyoul/fed_finbert.git",
+    license="Apache",
+    packages=find_packages()
+)
+
