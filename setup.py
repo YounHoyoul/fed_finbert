e855414b322b4536e8cdef5f7fef680c12b25d74
Hoyoul Youn <yhy0215@gmail.com>
2021-05-03 10:34:24 +1000

    change the folder name from fed_finbert to fednlp

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nlp_fed",
+    name="fednlp",
     version="0.1.0",
     author="Hoyoul Youn",
     author_email="yhy0215@gmail.com",
     url="https://github.com/YounHoyoul/fed_nlp.git",
     license="Apache",
-    packages=['nlp_fed'],
+    packages=['fednlp'],
     install_requires=[
         'torch==1.6.0',
         'torchvision==0.7.0',
